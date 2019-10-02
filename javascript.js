4c8f7bc53c2b897e6679cc39b797da415e3d3318
thanhle <mix2pigcat@gmaail.com>
2019-10-02 15:24:44 +0700

    filebai11

---
+++ javascript.js
@@ -0,0 +1,8 @@
+let a=parseInt(prompt('So thang lao dong: '));
+if (a<12){
+    alert('Thue thu nhap tinh theo luy tien tung phan ')
+}else if(a<3){
+    alert('Thue thu nhap ca nhan la 10% tong thu nhap');
+}else {
+    alert('Thue thu nhap ca nhan la 20% tong thu nhap')
+}
